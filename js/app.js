40a23ff7f9cadf1d2e38ef298ee57edc54ecd098
Erica Bee <ebee511@gmail.com>
2020-01-12 19:43:25 -0700

    add guess logic, reset function, and vars

--- js/app.js
+++ js/app.js
@@ -1,36 +1,56 @@
-// Guess a number in the range of 1 to 10
-// Computer has to randomly pick a number each round
-// Grab the user input and check to see if number is in the array
-// Reset correct number
+// Computer chooses random num between 1 and 100
+// User guesses number between 1 and 100 ten times
+// After each guess, player should be told if they are right/wrong, high/low
+// Display previously guessed numbers
+// Game ends after correct number or run out of turns
+// When game is over, new game option is presented
 
-// Variables
-let userGuessInput = document.getElementById("user-guess");
+// VARIABLE
+let userGuess = document.getElementById("user-guess");
 let submitBtn = document.getElementById("submitBtn");
-let correctNumber;
+let resetBtn = document.getElementById("resetBtn");
+let correctNum = Math.floor(Math.random() * 100) + 1;
+let guessArray = [];
+let guessCount = 0;
 
-// Event Listener
-submitBtn.addEventListener("click", gameRound);
+console.log(correctNum);
 
-// Functions
-function gameRound(e) {
-  // prevent reload of page
-  e.preventDefault();
-  // generate CPU number
-  correctNumber = getRandomIntInclusive(0, 10);
-  console.log(correctNumber);
-  // grab user number
-  userGuess = userGuessInput.value;
-  console.log(userGuess);
+// on each submit guess, increase guess count
+submitBtn.addEventListener("click", function() {
+  // increase guess count to monitor turns
+  guessCount++;
 
-  if (userGuess !== correctNumber) {
-    console.log("You are wrong.");
+  // grab num value of guess, push into array
+  userGuessNum = parseInt(userGuess.value);
+  guessArray.push(userGuessNum);
+  console.log(guessArray);
+  userGuess.value = "";
+
+  // compare user guess to correct number
+  if (userGuessNum > correctNum) {
+    console.log("Your guess is too high");
+  } else if (userGuessNum < correctNum) {
+    console.log("Your guess is too low");
   } else {
-    console.log("You are right!");
+    console.log("You are correct!");
   }
-}
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  // use if statement to control game round
+  if (guessCount === 9) {
+    alert("Game over!");
+    submitBtn.classList.add("hide");
+    resetBtn.classList.remove("hide");
+    resetBtn.classList.add("show");
+  }
+});
+
+resetBtn.addEventListener("click", resetGame);
+
+function resetGame() {
+  guessCount = 0;
+  guessArray = [];
+  submitBtn.classList.remove("hide");
+  submitBtn.style.display = "inline-block";
+  resetBtn.classList.remove("show");
+  resetBtn.classList.add("hide");
 }
