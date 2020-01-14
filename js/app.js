5bcb4f105c9505c77440824f3a5e850d9f634a83
Erica Bee <ebee511@gmail.com>
2020-01-13 20:05:47 -0700

    add DOM display message and refactor code

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@
 let userGuess = document.getElementById("user-guess");
 let submitBtn = document.getElementById("submitBtn");
 let resetBtn = document.getElementById("resetBtn");
+let displayGuessesUI = document.getElementById("guesses");
+let displayMessageUI = document.getElementById("display-message");
 let correctNum = Math.floor(Math.random() * 100) + 1;
 let guessArray = [];
 let guessCount = 0;
@@ -24,23 +26,30 @@ submitBtn.addEventListener("click", function() {
   userGuessNum = parseInt(userGuess.value);
   guessArray.push(userGuessNum);
   console.log(guessArray);
+  displayGuessesUI.textContent = guessArray;
   userGuess.value = "";
 
   // compare user guess to correct number
   if (userGuessNum > correctNum) {
     console.log("Your guess is too high");
+    displayMessageUI.textContent = "Your guess is too high";
+    displayMessageUI.style.backgroundColor = "red";
   } else if (userGuessNum < correctNum) {
     console.log("Your guess is too low");
+    displayMessageUI.textContent = "Your guess is too low";
+    displayMessageUI.style.backgroundColor = "red";
   } else {
     console.log("You are correct!");
+    displayMessageUI.textContent = "Correct! You win!";
+    displayMessageUI.style.backgroundColor = "green";
+    showResetBtn();
   }
 
   // use if statement to control game round
   if (guessCount === 9) {
-    alert("Game over!");
-    submitBtn.classList.add("hide");
-    resetBtn.classList.remove("hide");
-    resetBtn.classList.add("show");
+    displayMessageUI.textContent = `No more guesses. The correct number was ${correctNum}. Game over!`;
+    displayMessageUI.style.backgroundColor = "yellow";
+    showResetBtn();
   }
 });
 
@@ -53,4 +62,16 @@ function resetGame() {
   submitBtn.style.display = "inline-block";
   resetBtn.classList.remove("show");
   resetBtn.classList.add("hide");
+
+  displayMessageUI.textContent = "";
+
+  correctNum = Math.floor(Math.random() * 100) + 1;
+  console.log(correctNum);
+}
+
+// Runs to hide submit btn and display play again btn
+function showResetBtn() {
+  submitBtn.classList.add("hide");
+  resetBtn.classList.remove("hide");
+  resetBtn.classList.add("show");
 }
